92b0dfb7d0a52b22228a61dcd94c79488b56208d
ebmcambodia <ebmcambodia069@gmail.com>
2025-01-02 16:41:02 +0700

    EBMAPP

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Apps/LoginScreen';
+import { client } from './Apps/Utils/KindConfig';
 
 export default function App() {
+
+  useEffect(() => {
+    checkAuthenticate();
+  },[]);
+
+  const checkAuthenticate = async () => {
+    // Using `isAuthenticated` to check if the user is authenticated or not
+    if (await client.isAuthenticated) {
+      const userProfile =await client.getUserDetails();
+      console.log(userProfile);
+      console.log("Authenticated!!!")
+      // Need to implement, e.g: call an api, etc...
+    } else {
+      // Need to implement, e.g: redirect user to sign in, etc..
+    }
+  };
+  
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginScreen/>
     </View>
   );
 }
@@ -14,7 +35,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
---
+++ Apps/Utils/Colors.js
@@ -0,0 +1,5 @@
+export default{
+    PRIMARY: '#FE9900',
+    GRAY:'#c0c0c0',
+    WHITE:'#fff'
+}
